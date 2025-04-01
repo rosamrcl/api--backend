6d85b0353bb37648dc9f81982e6a158116b35763
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-04-01 15:28:47 -0300

    .

---
+++ api/index.php
@@ -0,0 +1,27 @@
+<?php
+$data=[];
+
+//request
+if (isset($_GET['option'])){
+    switch($_GET['option']){
+        case 'status';
+        $data['status'] ='SUCESS';
+        $data['data'] = 'API running OK!';
+        break;
+
+        default:
+            $data['status']='ERROR';
+            break;
+    }else{
+        $data['status']='ERROR';
+    }
+}
+
+//response
+function response($data){
+    header("Content-Type:application/json");
+    echo json_encode($data);
+}
+
+
+?>
