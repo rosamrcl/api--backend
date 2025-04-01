0869c21a6e64d67b9ec89f2c9d5f6f9654e333b1
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-04-01 16:09:15 -0300

    .

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+define('API_BASE', 'http://localhost//api/api/index.php?option=');
+echo '<h1>APLICAÇÃO</h1><hr>';
+
+$resultado=api_request('status');
+print_r ($resultado);
+
+function api_request($option){
+    $cliente=curl_init(API_BASE .$option);
+    curl_setopt($cliente, CURLOPT_RETURNTRANSFER, true);
+    $response =curl_exec($cliente);
+    return json_decode($response,true);
+}
+
+?>
