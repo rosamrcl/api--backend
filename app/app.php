8e1937271f5cbfbec10a54fcf028b988ddadcf32
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-04-01 16:56:51 -0300

    .

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@ define('API_BASE', 'http://localhost//api/api/index.php?option=');
 echo '<h1>APLICAÇÃO</h1><hr>';
 
 $resultado=api_request('status');
+echo'<pre>'; // <pre> traz a informação do array mais organizada 
 print_r ($resultado);
 
 function api_request($option){
